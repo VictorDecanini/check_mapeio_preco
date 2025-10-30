ed3b6bcc9c69835e6f166c2ecfb77145ee823bdb
VictorDecanini <victor.decanini@scanntech.com>
2025-10-30 19:52:13 -0300

    Ajuste da aba resumo

--- check_mapeio_preco_streamlit.py
+++ check_mapeio_preco_streamlit.py
@@ -310,19 +310,13 @@ def to_excel_com_resumo(df, coluna_vendas):
             linha_atual = linha_inicial + i
             
             # Se chegamos à linha 8 (percentual), aplicamos o formato percentual
-
-            if linha_atual < 9:
-                worksheet.write(linha_atual - 1, 0, metrica, normal_format)
-
             if linha_atual == 9:
                 worksheet.write_number(linha_atual - 1, 1, valor / 100, percent_format)
             else:
                 worksheet.write(linha_atual - 1, 1, valor, number_format)
             
-            if linha_atual > 9:
-                worksheet.write(linha_atual, 0, metrica, normal_format)
-            # # Escreve a métrica na coluna A
-            # worksheet.write(linha_atual - 1, 0, metrica, normal_format)
+            # Escreve a métrica na coluna A
+            worksheet.write(linha_atual - 1, 0, metrica, normal_format)
 
         # Inserir linha vazia após a linha 8 (que será a linha 9)
         worksheet.write_blank(9, 0, None, normal_format)
@@ -339,7 +333,7 @@ def to_excel_com_resumo(df, coluna_vendas):
         worksheet.write("B9", df_resumo.loc[6, "Valor"] / 100, percent_format)  # Qtd total problemas
 
         worksheet.write("A13", "% Volume de vendas dos skus com possíveis problemas", orange_bold_format)
-        worksheet.write_number("B13", df_resumo.loc[9, "Valor"] / 100, percent_format)
+        worksheet.write_number("B13", df_resumo.loc[10, "Valor"] / 100, percent_format)
 
         # # 3️⃣ Remove destaque do “Volume de vendas total” e insere linha em branco antes do “com problema”
         # worksheet.write("A10", "", empty_format)
